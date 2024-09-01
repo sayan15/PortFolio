4e5726d1a04385c7c2d73182637eb92e200391ee
sayan15 <sayansaya95@gmail.com>
2024-09-01 15:55:43 +0100

    some more project added

--- src/app/_services/projects.service.ts
+++ src/app/_services/projects.service.ts
@@ -23,6 +23,24 @@ export class ProjectsService {
     projectlink:'https://github.com/sayan15/ReportMateMobile.git',
     tags:[Tag.Kotlin],
     pictures:["assets/Default/1.png","assets/Default/2.png","assets/Default/3.png"]
+  },
+  {
+    id:2,
+    name:'ReviewAppAssignment',
+    summary:'Review Mobile Application',
+    description:'Developed a comprehensive review application for a Northampton-based travel agency. The application includes both Android and web versions, offering users the ability to register, provide reviews about the agency’s services, like and dislike reviews from other users, and engage in conversations by replying to reviews. Users also have the flexibility to update or delete their reviews. Administrative privileges are granted, enabling administrators to delete and update inappropriate reviews.',
+    projectlink:'https://github.com/sayan15/ReviewAppAssignment',
+    tags:[Tag.Kotlin],
+    pictures:["assets/Default/1.png","assets/Default/2.png","assets/Default/3.png"]
+  },
+  {
+    id:3,
+    name:'Bill Generator',
+    summary:'Report Incident using Mobile Application',
+    description:'The system securely manages customer information, billing, and tariffs, with key features including secure admin login, customer registration, data updates, and confidential storage, ensuring robust data integrity and privacy.',
+    projectlink:'https://github.com/sayan15/BillGenrator',
+    tags:[Tag.JAVA],
+    pictures:["assets/Default/1.png","assets/Default/2.png","assets/Default/3.png"]
   }
 ];
 
